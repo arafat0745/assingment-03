f25bcff714e7f6318c5bb4fceec966359cb2adc7
unknown <shekhor1100@gmail.com>
2025-08-04 20:56:38 +0600

    problem03

---
+++ problem03.js
@@ -0,0 +1,12 @@
+/** Problem -03 ( Medicine Planner ) */
+var lastDay = 30 ;
+//write your code here
+
+for(var day = 1; day <= lastDay; day++){
+    if(day % 3 === 0){
+        console.log(day + " - madicine");
+    } else {
+        console.log(day + " - rest");
+    }
+
+}
