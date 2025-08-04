57c8de7fd7c2aaadec1e54b469c1fb75822ac0bb
unknown <shekhor1100@gmail.com>
2025-08-04 22:12:07 +0600

    final

--- final.js
+++ final.js
@@ -96,6 +96,31 @@ console.log(generateEmail);
 
 
 
+/** Problem 06 :  (Current Salary )  */
+var experience = 30;
+var startingSalary = 45000;
+//write your code here
+
+var currentSalary = startingSalary;
+var incrementRate =5/100;
+
+for(var i =1; i<= experience; i++){
+    currentSalary += currentSalary * incrementRate;
+}
+
+console.log(currentSalary.toFixed(2));
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
