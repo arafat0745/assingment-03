636a50fed9fa375c9f205c89158ed664f3039101
unknown <shekhor1100@gmail.com>
2025-08-04 19:56:40 +0600

    problem 02

--- problem01.js
+++ problem01.js
@@ -5,14 +5,3 @@ var area = 800;
 var landPerBrother = area/2;
 console.log(landPerBrother);
 
-/** Problem -02 ( Cycle or Laptop ) */
-var money = 25000;
-//write your code here
-
-if(money >=9999 ){
-console.log("laptop");
-} else if(money >=10000){
-console.log("cycle");
-} else{
-    console.log("chocolate");
-}
---
+++ problem02.js
@@ -0,0 +1,11 @@
+/** Problem -02 ( Cycle or Laptop ) */
+var money = 10000;
+//write your code here
+
+if(money >=25000 ){
+console.log("laptop");
+} else if(money >=10000){
+console.log("cycle");
+} else{
+    console.log("chocolate");
+}
