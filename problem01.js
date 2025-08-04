03570bde399c087551587b0d241f43f97d568bc9
unknown <shekhor1100@gmail.com>
2025-08-04 19:44:00 +0600

    assignment03 first

---
+++ problem01.js
@@ -0,0 +1,18 @@
+/** Problem -01 ( Divide the Asset ) */
+var area = 800;
+//write your code here
+
+var landPerBrother = area/2;
+console.log(landPerBrother);
+
+/** Problem -02 ( Cycle or Laptop ) */
+var money = 25000;
+//write your code here
+
+if(money >=9999 ){
+console.log("laptop");
+} else if(money >=10000){
+console.log("cycle");
+} else{
+    console.log("chocolate");
+}
